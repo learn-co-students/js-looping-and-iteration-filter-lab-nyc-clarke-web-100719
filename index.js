52dc1f02db37441084f71017991dc4735daac76d
GeneHFang <ghl234@nyu.edu>
2019-11-18 00:29:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file
+function findMatching(drivers, driverName){
+    return drivers.filter(function (name) {return name.toLowerCase() === driverName.toLowerCase();});
+}
+
+function fuzzyMatch(drivers, driverName){
+    let num = driverName.length
+    return drivers.filter(function (name) {return name.toLowerCase().substring(0,num)===driverName.toLowerCase();});
+
+}
+
+function matchName(driverObject, driverName){
+    return driverObject.filter(function (object) {return object.name.toLowerCase() === driverName.toLowerCase();});
+
+}
