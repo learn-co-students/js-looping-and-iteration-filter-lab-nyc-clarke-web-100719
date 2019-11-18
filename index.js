1d378e7c7fe0be28ac713fedc6d9a1e89c4e3b2d
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2019-11-18 16:13:09 -0500

    Done.

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function fuzzyMatch(drivers, string) {
 function matchName(drivers, string) {
     return drivers.filter (
         function (driver) {
-            return driver.name === string;
+            return driver.name.toLowerCase() === string.toLowerCase();
         }
     )
 
