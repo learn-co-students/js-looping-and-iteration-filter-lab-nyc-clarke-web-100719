2dc54b004326ecdbce0116f0de6ef0249c3d4869
gnardinosaur <github email address>
2019-11-18 08:20:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file
+function findMatching (arr, string) {
+    let match = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].toLowerCase() === string.toLowerCase()) {
+            match.push(arr[i]);
+        }
+    }
+    return match;
+}
+
+function fuzzyMatch (arr, string) {
+    let match = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i][0] === string[0]) {
+            match.push(arr[i]);
+        }
+    }
+    return match;
+}
+
+function matchName (arr, string) {
+    let match = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].name === string) {
+            match.push(arr[i]);
+        }
+    }
+    return match;
+}
