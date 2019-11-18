c6a1699d69fb5247a1265d6aeec28ee52b700e7b
soufianeou <soufiane.oucherrou@gmail.com>
2019-11-18 11:51:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file
+function findMatching(arr, target) {
+    let answer = [];
+    for (const ele in arr) {
+        if (arr[ele].charAt(0).toUpperCase() + arr[ele].slice(1) === target) {
+            answer.push(arr[ele]);
+        }
+    }
+    
+    return answer;
+}
+
+
+function fuzzyMatch(arr, chara) {
+    let answer = [];
+    for (const ele in arr) {
+        if (arr[ele].slice(0,2) === chara) {
+            answer.push(arr[ele]);
+        }
+    }
+    return answer;
+}
+
+function matchName (list, name) {
+    return list.filter(function (driver) {
+      return driver.name.toLowerCase() === name.toLowerCase();
+    });
+  }
